4bc55cd842f18ba25c5800b4a215eda92a168238
*** <res@163.com>
2020-11-25 10:39:22 +0800

    后台主页

--- assets/js/ajaxBase.js
+++ assets/js/ajaxBase.js
@@ -4,5 +4,19 @@ $.ajaxPrefilter(function (options) {
   // 在每次jQ发送ajax请求前会执行该函数，通过该函数的形参options可以获取到每次ajax的配置项
   // 来修改每次请求的配置项
   options.url = "http://ajax.frontend.itheima.net" + options.url;
-  console.log(options.url);
+  // console.log(options.url);
+  if (options.url.indexOf("/my/") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token"),
+    };
+  }
+  options.complete = function (xhr) {
+    if (
+      xhr.responseJSON.status === 1 &&
+      xhr.responseJSON.message === "身份认证失败!"
+    ) {
+      localStorage.removeItem("token");
+      location.href = "login.html";
+    }
+  }
 });
---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+$(function () {
+  let layer = layui.layer;
+  $.ajax({
+    url: "/my/userinfo",
+    // headers: {
+    //   Authorization: localStorage.getItem("token"),
+    // },
+    success: function (res) {
+      console.log(res);
+      if (res.status !== 0) {
+        return layer.msg("获取用户信息失败！")
+      }
+      // layer.msg("获取用户信息成功！");
+
+      let name = res.data.nickname || res.data.username;
+      let first = name[0].toUpperCase();
+
+      $("#welcom").text("欢迎" + name);
+
+      if (res.data.user_pic) {
+        $(".layui-nav-img").show().attr("src", res.data.user_pic);
+        $(".text-avatar").hide();
+      } else {
+        $(".layui-nav-img").hide();
+        $(".text-avatar").text(first).show();
+      }
+    },
+  });
+  $("#logoutBtn").click(function () {
+    layer.confirm(
+      '确定退出登录?',
+      { icon: 3, title: '提示' },
+      function (index) {
+        localStorage.removeItem("token");
+
+        location.href = "loogin.html";
+
+        layer.close(index);
+      }
+    );
+  });
+}); 
